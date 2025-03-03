b4e71da228bb702464ed135fdc9bfc8e34388ae2
0xGRAV3R <ycopyart.xyz@gmail.com>
2025-03-02 17:37:22 -0800

    ⚙️ Iterate and display the lends in the UI

--- src/components/Lend.tsx
+++ src/components/Lend.tsx
@@ -79,34 +79,50 @@ export const Lend: FC = () => {
     }
 
     return (
-        <div className="flex flex-row justify-center">
-            <div className="relative group items-center">
-                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
-                rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-                <button
-                    className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                    onClick={createLend} 
-                >
-                    <div className="hidden group-disabled:block">
-                        Wallet not connected
-                    </div>
-                    <span className="block group-disabled:hidden" > 
-                        Create Lend 
-                    </span>
-                </button>
+        
+        <div>
+            {
+                banks.map((bank) => {
+                    return (
+                        <div className='md:hero-content flex flex-col'>
+                            <h1>{bank.name.toString()}</h1>
+                            <span>{bank.balance.toString()}</span>
+                            
+                        </div>
+                    )
+                })
+            }
+        
+            <div className="flex flex-row justify-center">
+                <div className="relative group items-center">
+                    <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
+                    rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+                    <button
+                        className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                        onClick={createLend} 
+                    >
+                        <div className="hidden group-disabled:block">
+                            Wallet not connected
+                        </div>
+                        <span className="block group-disabled:hidden" > 
+                            Create Lend 
+                        </span>
+                    </button>
 
-                <button
-                    className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                    onClick={getLends} 
-                >
-                    <div className="hidden group-disabled:block">
-                        Wallet not connected
-                    </div>
-                    <span className="block group-disabled:hidden" > 
-                        Fetch Lends 
-                    </span>
-                </button>
+                    <button
+                        className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                        onClick={getLends} 
+                    >
+                        <div className="hidden group-disabled:block">
+                            Wallet not connected
+                        </div>
+                        <span className="block group-disabled:hidden" > 
+                            Fetch Lends 
+                        </span>
+                    </button>
+                </div>
             </div>
+
         </div>
     );
 };
